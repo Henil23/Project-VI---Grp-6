5ae6095cb09b9599bf40c1161e72de4840ca89d3
alexfridman <afridman0793@conestogac.on.ca>
2025-01-18 21:14:04 -0500

    basic styling and signin page created

---
+++ JobApplicationPortal/Controllers/SignIn.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JobApplicationPortal.Controllers
+{
+    public class SignIn : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
